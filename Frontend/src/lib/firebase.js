5e16b31862ce6d87dc514219b1fa6a5ca3943353
Elarionitis <suhanramani@gmail.com>
2026-04-07 22:07:42 +0530

    Google Auth added

--- Frontend/src/lib/firebase.js
+++ Frontend/src/lib/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+console.log("[Firebase Debug] Config:", firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
